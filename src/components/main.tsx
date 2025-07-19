15525d2cb24b94d5a1731063878f5c0e825fb775
Azizbek Uzgenboev <elemahunovbahrombek@gmail.com>
2025-07-19 14:37:40 +0500

    last-of

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -11,14 +11,14 @@ function Main() {
             <button className="btn-lin "></button>
             <div className="flex gap-[10px]">
               <div className="bg-[url('/Container(4).png')] bg-cover bg-center h-[300px] w-[50%] mt-[45px] rounded-[10px]">
-                <div className="gap-8 p-6 max-w-md ">
+                <div className="gap-8  max-w-md ">
                   <button className="text-1xl text-white bg-slate-400 w-[80px] h-[30px] mt-[80px]">
                     Racing
                   </button>
                   <h3 className="text-2xl font-[inter] gap-3 mt-3 text-white ">
                     Racing Games Browned Butte roadert Cookies Daily Breakfast
                   </h3>
-                  <div className="flex text-gray-400 gap-4 flex-wrap mt-3 items-center w-full">
+                  <div className="flex text-gray-400 gap-3 flex-wrap mt-3 items-center w-full">
                     <RxAvatar />
                     <span>BY ADMIN</span>
                     <MdOutlineDateRange />
@@ -36,7 +36,7 @@ function Main() {
                   <h3 className="text-2xl font-[inter] gap-3 mt-3 text-white ">
                     Racing Games Browned Butte roadert Cookies Daily Breakfast
                   </h3>
-                  <div className="flex text-gray-400 gap-4 flex-wrap mt-3 items-center w-full">
+                  <div className="flex text-gray-400 gap-3 flex-wrap mt-3 items-center w-full">
                     <RxAvatar />
                     <span>BY ADMIN</span>
                     <MdOutlineDateRange />
@@ -47,7 +47,7 @@ function Main() {
                 </div>
               </div>
             </div>
-            <div className="flex gap-4 mt-8 w-[100%] h-[80%] ">
+            <div className="flex gap-3 mt-8 w-[100%] h-[80%] ">
               <div className="bg-[url('/Container(1).png')] rounded-[20px] bg-cover bg-center w-[400px] h-[300px] items-center">
                 <button className="items-center flex ml-6 bg-slate-400 text-black mt-[100px]">
                   Fighter
@@ -101,15 +101,122 @@ function Main() {
           <div className="relative mt-[70px] mb-10 z-999 w-[20%] bg-white ml-32 ">
             <button className="bg-[#F4796C] btn ">Recent Posts</button>
             <button className="btn-lin "></button>
-            <div className="mt-9">
-                <img src="/racing.jpg" alt="" className="w-[100px] h-[100px]" />
-              <div className="flex gap-4 flex-col">
-                <button>Racing</button>
-                <h4>The Butter Chocolate Cookies Daily</h4>
-                
+            <div className="mt-9 ml-4 flex items-start gap-3  bg-white rounded-xl shadow-md w-[90%] max-w-md">
+              <img
+                src="/racing.jpg"
+                alt="Racing"
+                className="w-[100px] h-[100px] object-cover rounded-lg"
+              />
+
+              <div className="flex flex-col justify-between">
+                <button className="text-sm text-white bg-blue-600 px-3 py-1 rounded-md w-fit hover:bg-blue-700 transition">
+                  Racing
+                </button>
+
+                <h4 className="text-lg font-semibold text-gray-800 mt-2">
+                  The Butter Chocolate Cookies Daily
+                </h4>
+
+                <div className="flex items-center text-gray-500 text-sm mt-2 gap-3 flex-wrap">
+                  <div className="flex items-center gap-1">
+                    <MdOutlineDateRange className="text-lg" />
+                    <span>27 August, 2024</span>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <MdOutlineUpdate className="text-lg" />
+                    <span>20 mins</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-9 ml-4 flex items-start gap-3  bg-white rounded-xl shadow-md w-[90%] max-w-md">
+              <img
+                src="/racing.jpg"
+                alt="Racing"
+                className="w-[100px] h-[100px] object-cover rounded-lg"
+              />
+
+              <div className="flex flex-col justify-between">
+                <button className="text-sm text-white bg-blue-600 px-3 py-1 rounded-md w-fit hover:bg-blue-700 transition">
+                  Racing
+                </button>
+
+                <h4 className="text-lg font-semibold text-gray-800 mt-2">
+                  The Butter Chocolate Cookies Daily
+                </h4>
+
+                <div className="flex items-center text-gray-500 text-sm mt-2 gap-3 flex-wrap">
+                  <div className="flex items-center gap-1">
+                    <MdOutlineDateRange className="text-lg" />
+                    <span>27 August, 2024</span>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <MdOutlineUpdate className="text-lg" />
+                    <span>20 mins</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-9 ml-4 flex items-start gap-3  bg-white rounded-xl shadow-md w-[90%] max-w-md">
+              <img
+                src="/racing.jpg"
+                alt="Racing"
+                className="w-[100px] h-[100px] object-cover rounded-lg"
+              />
+
+              <div className="flex flex-col justify-between">
+                <button className="text-sm text-white bg-blue-600 px-3 py-1 rounded-md w-fit hover:bg-blue-700 transition">
+                  Racing
+                </button>
+
+                <h4 className="text-lg font-semibold text-gray-800 mt-2">
+                  The Butter Chocolate Cookies Daily
+                </h4>
+
+                <div className="flex items-center text-gray-500 text-sm mt-2 gap-3 flex-wrap">
+                  <div className="flex items-center gap-1">
+                    <MdOutlineDateRange className="text-lg" />
+                    <span>27 August, 2024</span>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <MdOutlineUpdate className="text-lg" />
+                    <span>20 mins</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-9 ml-4 flex items-start gap-3  bg-white rounded-xl shadow-md w-[90%] max-w-md">
+              <img
+                src="/racing.jpg"
+                alt="Racing"
+                className="w-[100px] h-[100px] object-cover rounded-lg"
+              />
+
+              <div className="flex flex-col justify-between">
+                <button className="text-sm text-white bg-blue-600 px-3 py-1 rounded-md w-fit hover:bg-blue-700 transition">
+                  Racing
+                </button>
+
+                <h4 className="text-lg font-semibold text-gray-800 mt-2">
+                  The Butter Chocolate Cookies Daily
+                </h4>
+
+                <div className="flex items-center text-gray-500 text-sm mt-2 gap-3 flex-wrap">
+                  <div className="flex items-center gap-1">
+                    <MdOutlineDateRange className="text-lg" />
+                    <span>27 August, 2024</span>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <MdOutlineUpdate className="text-lg" />
+                    <span>20 mins</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+        <div className="mt-10 flex justify-center">
+            <img src="/advertisement04.jpg.png" alt="" />
         </div>
       </div>
     </>
